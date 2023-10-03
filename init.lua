8493491f9154c229eb8f73d301948be594116e78
Yannik Daellenbach <yannik@daellenbach.org>
2023-10-03 12:35:35 +0200

    Configure LSP servers

--- init.lua
+++ init.lua
@@ -637,6 +637,18 @@ require('lazy').setup({
             },
           },
         },
+        clangd = {},
+        golangci_lint_ls = {},
+        pyright = {},
+        rust_analyzer = {},
+        tsserver = {},
+        html = { filetypes = { 'html', 'twig', 'hbs' } },
+        tailwindcss = {},
+        texlab = {},
+        yamlls = {},
+        bashls = {},
+        vale_ls = {},
+        sqlls = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -901,7 +913,25 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'go',
+        'rust',
+        'python',
+        'tsx',
+        'typescript',
+        'javascript',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
