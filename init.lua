c34afe7436c3bc2d6e11805260c7f42a707941fe
Yannik Daellenbach <git@daellenbach.org>
2026-02-28 17:34:33 +0100

    Configure `nixfmt`

--- init.lua
+++ init.lua
@@ -657,6 +657,7 @@ require('lazy').setup({
         bashls = {},
         vale_ls = {},
         sqlls = {},
+        nixfmt = {},
       }
 
       -- Ensure the servers and tools above are installed
